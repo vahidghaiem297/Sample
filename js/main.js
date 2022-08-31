80e922c9b331a910c1b9205d18ddbe783aebcb2e
Vahid Ghaiem <vahidghaiem297@gmail.com>
2022-08-31 18:03:09 +0430

    initial project

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+$('#foot-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:12
+        }
+    }
+});
